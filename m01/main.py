5a6158f8cdaeb9a47f011a89da23da74979b6c56
Ben Chen <idben.chen@gmail.com>
2026-07-16 09:23:49 +0800

    模組化的第一個範例

---
+++ m01/main.py
@@ -0,0 +1,4 @@
+import spell_tools
+
+result = spell_tools.get_spell_label("荷伊米", 3)
+print(result)
---
+++ m01/spell_tools.py
@@ -0,0 +1,2 @@
+def get_spell_label(spell_name: str, mp_cost: int) -> str:
+    return f"{spell_name} 需要 {mp_cost} MP"

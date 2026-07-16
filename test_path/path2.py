9da449c4a026a678da5357ce3fbc40d4e9ece2e7
Ben Chen <idben.chen@gmail.com>
2026-07-16 16:27:34 +0800

    建立寫檔目的地的上層路徑

--- test_path/path2.py
+++ test_path/path2.py
@@ -2,3 +2,10 @@ from pathlib import Path
 
 write_ptah = Path("aa1234.txt")
 write_ptah.write_text("Today...", encoding="utf-8")
+
+# 建立資料夾
+working_path = Path("yy123")
+write_ptah2 = working_path / "aa" / "bb" / "bb345.txt"
+# 建立寫檔目的地的上層路徑
+write_ptah2.parent.mkdir(parents=True, exist_ok=True)
+write_ptah2.write_text("test test 123", encoding="utf-8")

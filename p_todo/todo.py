9db3bbf1d14a4abc068132d1faef683f57fa8362
Ben Chen <idben.chen@gmail.com>
2026-07-16 10:49:23 +0800

    待辦事項: Todo

--- p_todo/todo.py
+++ p_todo/todo.py
@@ -1,2 +1,22 @@
 class Todo:
-    pass
+    # title: 待辦事項的文字
+    # isFinished: 是否完成待辦事項
+    # createAt: 建立日期
+    # isDel: 軟刪除用的屬性
+    def __init__(self, title: str) -> None:
+        self.title = title
+        self.isFinished = False
+        self.isDel = False
+    def __str__(self) -> str:
+        mark = " "
+        if self.isFinished:
+            mark = "v"
+        return f"[{mark}] {self.title}"
+
+def main():
+    todo01 = Todo("買雞胸肉")
+    todo01.isFinished = True
+    print(todo01)
+
+if __name__ == "__main__":
+    main()
--- p_todo/todolist.py
+++ p_todo/todolist.py
@@ -1,11 +1,23 @@
+from todo import Todo
+
 class Todolist:
-    pass
+    def __init__(self, title: str) -> None:
+        self.title = title
+        self.todos: list[Todo] = []
     # 建立待辦事項
+    def add(self, title: str) -> None:
+        self.todos.append(Todo(title))
     # 刪除待辦事項 (真的刪除)
     # 顯示所有的待辦事項
     #   [ ] 買雞蛋
-    #   [x] 買雞蛋
+    #   [v] 買雞蛋
     # 打叉的方法
     # 去掉打叉的方法
     # 存檔
     # 讀檔
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

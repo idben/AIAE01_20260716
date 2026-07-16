3b099e285b97b1dd7bf6ca21e9ca5a212013a78f
Ben Chen <idben.chen@gmail.com>
2026-07-16 09:28:03 +0800

    from import 的載入方式

--- m01/main.py
+++ m01/main.py
@@ -1,4 +1,9 @@
 import spell_tools
+# import pokestop_tools
+from pokestop_tools import get_reward_label
 
 result = spell_tools.get_spell_label("荷伊米", 3)
 print(result)
+
+result2 = get_reward_label("大師球", 1)
+print(result2)
---
+++ m01/pokestop_tools.py
@@ -0,0 +1,2 @@
+def get_reward_label(item_name: str, amount: int) -> str:
+    return f"補給站取得 {amount} 個 {item_name}"

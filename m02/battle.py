5304eaa682ab9576d6e69dc9b790009801364243
Ben Chen <idben.chen@gmail.com>
2026-07-16 10:22:30 +0800

    ch7-05 操作

--- m02/battle.py
+++ m02/battle.py
@@ -8,3 +8,13 @@ SPELL_MP_COSTS: dict[str, int] = {
 def cast_spell(caster: PartyMember, target: PartyMember, damage: int) -> str:
     target.take_damage(damage)
     return f"{caster.name} 施放咒文，{target.name} 剩下 {target.hp} HP"
+
+
+def main():
+    ch01 = PartyMember("小美", 100)
+    ch02 = PartyMember("骨頭怪", 20)
+    print(cast_spell(ch01, ch02, SPELL_MP_COSTS["荷依米"]))
+
+
+if __name__ == "__main__":
+    main()
